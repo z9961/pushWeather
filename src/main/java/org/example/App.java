a32d6c612d1f9333e845141a7ddb53aa546a7bcb
孟文杰 <449014690@qq.com>
2021-01-15 13:50:54 +0800

    init

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,6 +12,7 @@ import java.util.concurrent.TimeUnit;
  * Hello world!
  */
 public class App {
+
     public static String main(KeyValueClass kv) {
         String qurl = "https://www.qdfd.com.cn/qdweb/realweb/fh/FhHouseDetail.jsp?houseID=35000006760632";
         Request request = new Request.Builder().url(qurl)
@@ -22,11 +23,11 @@ public class App {
             ResponseBody responseBody = client.newCall(request).execute().body();
             String string = responseBody.string();
             String s1 = string.substring(string.indexOf("状态"));
-            s1 = s1.substring(s1.indexOf("st3\">") + 5);
+            s1 = s1.substring(s1.indexOf("st3\">") + 5).substring(0,3);
 
             if (s1.equals("已签约")) {
                 Date date = new Date();
-                if (date.getHours() < 9 || date.getHours() > 20 || date.getHours() % 3 != 0) {
+                if (date.getHours() < 9 || date.getHours() > 20 || date.getHours() % 2 != 1) {
                     return s1;
                 }
             }
@@ -34,13 +35,19 @@ public class App {
             s1 = "正阳府进展：" + s1.substring(0, 3);
             System.out.println(s1);
 
-            qurl = "https://api.htm.fun/api/Wechat/text/?corpid=wwdc1f851ff6ccb4f4&corpsecret" +
-                    "=sKX2XcBAP_EL4e1R0mdn9If0g5Lhm5nzyMb1pmK3xrA&agentid=1000002&text=" + s1;
+            qurl = "http://api.qqpusher.yanxianjun.com/send_private_msg?token=cd3eac5f85352b8064628ead2fe59972" +
+                    "&user_id=449014690&message=" + s1 + "&auto_escape=true";
             request = new Request.Builder().url(qurl)
                     .get().build();
-            client = new OkHttpClient().newBuilder().connectTimeout(50, TimeUnit.SECONDS).readTimeout(120,
-                    TimeUnit.SECONDS).build();
-            client.newCall(request).execute();
+            client = new OkHttpClient().newBuilder()
+                    .connectTimeout(50, TimeUnit.SECONDS)
+                    .readTimeout(120, TimeUnit.SECONDS).build();
+            try {
+                client.newCall(request).execute();
+                return "success";
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             return s1;
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/org/example/main.java
+++ src/main/java/org/example/main.java
@@ -6,6 +6,7 @@ package org.example;
  */
 public class main {
     public static void main(String[] args) {
-        System.out.println(1);
+        App app = new App();
+        App.main(null);
     }
 }
