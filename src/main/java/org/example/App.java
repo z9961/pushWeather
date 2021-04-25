a3ea5d639f471177815a2ee0aa7a467521f9f07e
孟文杰 <449014690@qq.com>
2021-04-25 14:17:47 +0800

    换个接口

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,13 @@
 package org.example;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.ResponseBody;
 
 import java.io.IOException;
-import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,41 +16,34 @@ import java.util.concurrent.TimeUnit;
 public class App {
 
     public static String main(KeyValueClass kv) {
-        String qurl = "https://www.qdfd.com.cn/qdweb/realweb/fh/FhHouseDetail.jsp?houseID=35000006760632";
+        String qurl = "https://tianqiapi.com/api?version=v6&appid=25174476&appsecret=ToEVL60S&cityid=101120211";
         Request request = new Request.Builder().url(qurl)
                 .get().build();
-        OkHttpClient client = new OkHttpClient().newBuilder().connectTimeout(50, TimeUnit.SECONDS).readTimeout(120,
-                TimeUnit.SECONDS).build();
+        OkHttpClient client = new OkHttpClient().newBuilder().connectTimeout(50, TimeUnit.SECONDS)
+                .readTimeout(120, TimeUnit.SECONDS).build();
         try {
             ResponseBody responseBody = client.newCall(request).execute().body();
             String string = responseBody.string();
-            String s1 = string.substring(string.indexOf("状态"));
-            s1 = s1.substring(s1.indexOf("st3\">") + 5).substring(0,3);
+            JSONObject json = JSON.parseObject(string);
+            StringBuilder s = new StringBuilder();
+            s.append("今天").append(json.get("week")).append(",").append(json.get("wea")).append(",")
+                    .append("最低").append(json.get("tem2")).append("℃,最高").append(json.get("tem")).append("℃,").append(json.get(
+                    "win")).append(json.get("win_speed")).append(",").append(json.get("air_tips").toString().replace(
+                    "，", ","));
 
-            if (s1.equals("已签约")) {
-                Date date = new Date();
-                if (date.getHours() < 9 || date.getHours() > 20 || date.getHours() % 2 != 1) {
-                    return s1;
-                }
-            }
-
-            s1 = "正阳府进展：" + s1.substring(0, 3);
-            System.out.println(s1);
-
-            qurl = "http://api.qqpusher.yanxianjun.com/send_private_msg?token=cd3eac5f85352b8064628ead2fe59972" +
-                    "&user_id=449014690&message=" + s1 + "&auto_escape=true";
+            qurl = "https://push.xuthus.cc/group/e5327555e79e21682592c46f5d9f2140?c=" + s.toString();
             request = new Request.Builder().url(qurl)
                     .get().build();
             client = new OkHttpClient().newBuilder()
                     .connectTimeout(50, TimeUnit.SECONDS)
                     .readTimeout(120, TimeUnit.SECONDS).build();
             try {
-                client.newCall(request).execute();
+                Response execute = client.newCall(request).execute();
                 return "success";
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            return s1;
+            return s.toString();
         } catch (IOException e) {
             e.printStackTrace();
         }
